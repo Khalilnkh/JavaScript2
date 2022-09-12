a50b4b481a91011dbc4b37187ff43d79e33956c2
Khalilnkh <khalilnkh@code.edu.az>
2022-09-12 10:48:05 +0400

    created

---
+++ assest/main.js
@@ -0,0 +1,39 @@
+//  var body=document.getElementById('bodyId')
+//  var message;
+//  if(confirm('Would you like to see the website?')==true){
+//      alert('You have permission')
+//      body.style.visibility='visible';
+//  }
+//  else{
+//      alert('You dont have permission')
+//      body.style.visibility='hidden';
+
+//  }
+
+// var person=prompt('Enter your age')
+//   if(person>18){
+//       alert('You have permission to enter website')
+//  }
+//   else{
+//       alert('You dont have permission because of your age')
+//   }
+
+//  function press(){
+//     if(body.style.color==white){
+//         body.style.color=black
+//    }
+//    else{
+//         body.style.color=white
+//     }
+//  }
+
+// function Group()
+// var value=document.getElementById('input').value
+// if(value.lenght===4 || value.lenght===5){
+//     if(value.includes('1' , 1))
+//     body.style.backgroundColor= "yellow"
+//     else if(value.includes('2' , 1))
+//     body.style.backgroundColor= "yellow"
+//     else if(value.includes('3' , 1))
+//     body.style.backgroundColor= "yellow"
+// }
